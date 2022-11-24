5107004ce3a56441a746f2d4664da0ed368ed5f4
sufailbadushat <sufailbadushat@gamil.com>
2022-11-24 13:01:44 +0530

    patient view page add and its router set

---
+++ src/app/patient-view/patient-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-view',
+  templateUrl: './patient-view.component.html',
+  styleUrls: ['./patient-view.component.css']
+})
+export class PatientViewComponent {
+
+}
