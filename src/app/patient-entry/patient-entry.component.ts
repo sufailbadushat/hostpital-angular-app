39c956eec52a63e0b4987c21f6b9f2b020a9db41
sufailbadushat <sufailbadushat@gamil.com>
2022-11-29 00:12:10 +0530

    read from form add

--- src/app/patient-entry/patient-entry.component.ts
+++ src/app/patient-entry/patient-entry.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./patient-entry.component.css']
 })
 export class PatientEntryComponent {
+  name = ""
+  id = ""
+  address = ""
+  contactNo = ""
+  dateOfAppinment = ""
+  image = ""
+  doctorName = ""
 
+  readValues = () => {
+    let data: any =
+    {
+      "name": this.name,
+      "id": this.id,
+      "address": this.address,
+      "contactNo": this.contactNo,
+      "dateOfAppinment": this.dateOfAppinment,
+      "image": this.image,
+      "doctorName": this.doctorName
+    }
+    console.log(data)
+  }
 }
